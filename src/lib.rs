875326ed7d8869f905f3864f1a6e0d17ba501f3a
liob <hbwinther@metalabs.de>
2023-03-14 18:12:08 +0100

    added some unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -14,10 +14,10 @@ pub use sampler::traits::ReSample;
 /// Corners could be calculated, e.g. using itertools. As we only need to cover the 3D
 /// usecase, we simply do it "manually".
 #[rustfmt::skip] // do not mangle manual matrix format
-fn get_corners(input_shape: &Vector3<usize>) -> MatrixXx3<usize>
+fn get_corners(in_shape: &Vector3<usize>) -> MatrixXx3<usize>
 where
 {
-    let is = input_shape;
+    let is = in_shape;
     MatrixXx3::<usize>::from_row_slice(&[
         0,         0,        0,
         0,         0,        is[2] - 1,
@@ -33,7 +33,7 @@ where
 /// Transform the point matrix by applying the affine transform + translation.
 fn apply_affine<T>(affine: &Matrix4<T>, pts: &MatrixXx3<T>) -> MatrixXx3<T>
 where
-    T: Num + Scalar + ClosedAdd + ClosedMul + Copy,
+    T: Num + Scalar + ClosedAdd + ClosedMul + Copy + Display,
 {
     let (aff, tra) = afftra_to_aff_tra(affine);
     let mut r = pts * aff.transpose();
@@ -194,5 +194,83 @@ where
     match sampler.sample(in_im, &out_coords, out_shape) {
         Ok(out_im) => Ok(out_im),
         Err(err) => Err(err),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use approx::*;
+
+    #[test]
+    #[rustfmt::skip] // do not mangle manual matrix format
+    fn test_get_corners() {
+        // normal use case:
+        let in_shape: Vector3<usize> = Vector3::from_vec(vec![256, 230, 16]);
+        let corners: MatrixXx3<usize> = MatrixXx3::from_row_slice(&[
+            0,   0,   0,
+            0,   0,   15,
+            0,   229, 0,
+            0,   229, 15,
+            255, 0,   0,
+            255, 0,   15,
+            255, 229, 0,
+            255, 229, 15,
+        ]);
+        assert_eq!(get_corners(&in_shape), corners);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_get_corners_invalid_shape() {
+        // edge case:
+        // shape dim cannot be 0
+        let in_shape: Vector3<usize> = Vector3::from_vec(vec![0, 1, 2]);
+        get_corners(&in_shape);
+    }
+
+    #[test]
+    #[rustfmt::skip] // do not mangle manual matrix format
+    fn test_apply_affine() {
+        let affine: Matrix4<f32> = Matrix4::from_row_slice(&[
+            0.0,  0.0, 6.7, 50.2,
+            0.0,  1.6, 0.0, -192.0,
+            -1.6, 0.0, 0.0, 177.9,
+            0.0,  0.0, 0.0, 1.0
+        ]);
+        let pts: MatrixXx3<f32> = MatrixXx3::from_row_slice(&[
+            12.0,    11.0,   0.0,
+            -113.1,  0.0,    555.5,
+            -1001.8, 3695.7, -0.01,
+        ]);
+        let exptected_result: MatrixXx3<f32> = MatrixXx3::from_row_slice(&[
+            50.2,      -174.4,  158.7,
+            3772.0498, -192.0,  358.86,
+            50.133,    5721.12, 1780.78,
+        ]);
+        assert_relative_eq!(apply_affine(&affine, &pts), exptected_result);
+    }
+
+    #[test]
+    #[rustfmt::skip] // do not mangle manual matrix format
+    fn test_vox2out_vox () {
+        let in_shape: Vector3<usize> = Vector3::from_vec(vec![256, 230, 16]);
+        let in_affine: Matrix4<f32> = Matrix4::from_row_slice(&[
+            0.0,  0.0,  6.7, 50.2,
+            0.0,  -1.6, 0.0, -192.0,
+            -1.6, 0.0,  0.0, 177.9,
+            0.0,  0.0,  0.0, 1.0
+        ]);
+        let voxel_sizes: Vector3<f32> = Vector3::from_vec(vec![1.5, 3.0, 6.0]);
+        let expected_out_shape: Vector3<usize> = Vector3::from_vec(vec![68, 124, 69]);
+        let expected_out_affine: Matrix4<f32> = Matrix4::from_row_slice(&[
+            1.5, 0.0, 0.0, 50.2,
+            0.0, 3.0, 0.0, -558.4,
+            0.0, 0.0, 6.0, -230.1,
+            0.0, 0.0, 0.0, 1.0
+        ]);
+        let (out_shape, out_affine) = vox2out_vox(&in_shape, &in_affine, &voxel_sizes);
+        assert_eq!(out_shape, expected_out_shape);
+        assert_relative_eq!(out_affine, expected_out_affine);
     }
 }
